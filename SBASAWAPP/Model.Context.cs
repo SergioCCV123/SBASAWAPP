185ca202e89328f8bdcfd7373bcb97c1e5880ed9
Sergio Calderon <sergiocalderonc12@gmail.com>
2022-02-24 09:51:43 -0600

    Plantilla y Inventario

--- SBASAWAPP/Model.Context.cs
+++ SBASAWAPP/Model.Context.cs
@@ -25,5 +25,12 @@ namespace SBASAWAPP
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<PRODUCT> PRODUCTS { get; set; }
+        public virtual DbSet<CATEGORIES> CATEGORIES { get; set; }
     }
 }
