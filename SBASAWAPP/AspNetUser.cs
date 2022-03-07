c8c78f01ecd363e0f2e86c265495456c31d76001
Eddy Villalobos <eddy.villalobos.che@gmail.com>
2022-03-07 08:21:14 -0600

    CRUD Factura
    Desarrollar el Crud factura y añadir la tabla factura a la bd

--- SBASAWAPP/AspNetUser.cs
+++ SBASAWAPP/AspNetUser.cs
@@ -20,6 +20,7 @@ namespace SBASAWAPP
             this.AspNetUserClaims = new HashSet<AspNetUserClaim>();
             this.AspNetUserLogins = new HashSet<AspNetUserLogin>();
             this.AspNetRoles = new HashSet<AspNetRole>();
+            this.FACTURA = new HashSet<FACTURA>();
         }
     
         public string Id { get; set; }
@@ -41,5 +42,7 @@ namespace SBASAWAPP
         public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AspNetRole> AspNetRoles { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<FACTURA> FACTURA { get; set; }
     }
 }
