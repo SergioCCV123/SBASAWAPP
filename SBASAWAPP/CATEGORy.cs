d0876f5813a191de6ca552406d4941439cd745a8
Eddy Villalobos <eddy.villalobos.che@gmail.com>
2022-04-28 17:52:43 -0600

    Validacion a categoria

--- SBASAWAPP/CATEGORy.cs
+++ SBASAWAPP/CATEGORy.cs
@@ -11,7 +11,8 @@ namespace SBASAWAPP
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class CATEGORy
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,9 @@ namespace SBASAWAPP
         }
     
         public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Categoria")]
         public string CATEGORY_NAME { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
